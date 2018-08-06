3d59cbad0610fc641801a5814b7a04ca19ad3b70
Jason Wu <lujason2015@gmail.com>
2018-08-05 23:21:06 -0400

    delimited sum

--- stringcalculatorkata.c
+++ stringcalculatorkata.c
@@ -1,4 +1,5 @@
-// WRITE HERE IF YOU'RE WORKING ON THIS!
+// WRITE HERE IF YOU'RE WORKING ON THIS! Jason: finished #3 comma delimited sum
+//Challenge link http://www.peterprovost.org/blog/2012/05/02/kata-the-only-way-to-learn-tdd/
 
 #include <stdio.h>
 #include <string.h>
@@ -8,6 +9,8 @@
 void testSingleDigit(void);
 void testEmptyStr(void);
 void testMultipleDigits(void);
+void testCommaDelimitedSum(void);
+
 int strCalc(char *input);
 
 
@@ -16,6 +19,7 @@ int main(void)
     testEmptyStr();
     testSingleDigit();
     testMultipleDigits();
+    testCommaDelimitedSum();
 }
 
 int strCalc(char *input){
@@ -23,16 +27,29 @@ int strCalc(char *input){
     {
         return 0;
     }
+    // printf("String: %s\n", input);
 
-    int strtoint = 0;
-    // int powerCounter = 0;
-    for(int i = strlen(input); i>0; i--){
+    int result = 0;
+    int powerIndex = 0;
+    int digitPointer = strlen(input)-1;
+    // printf("Power start index : %d\n", powerIndex);
 
-        strtoint += (input[i]-'0')*pow(10,powerCounter++);
+    for (int i = 0; i<strlen(input); i++)
+    {
+
+        if(input[digitPointer]==','){
+            powerIndex = 0; //restart powetIndex count
+            // printf("Comma found\n");
+            digitPointer--; //cannot be put in if statement
+        }else{
+            // printf("digitPointer: %d, powerIndex: %d\n", digitPointer, powerIndex);
+            result += (input[digitPointer--]-'0')*pow(10,powerIndex++); //add each digit to the sum according to its position
+            // printf("result: %d\n", result);
+        }
     }
 
-    printf("string to int: %i\n", strtoint);
-    return strtoint;
+    // printf("Final result : %i\n", result);
+    return result;
 }
 
 void testEmptyStr(void)
@@ -43,13 +60,18 @@ void testEmptyStr(void)
 
 void testSingleDigit(void)
 {
-    char *input = "1";
-    assert(strCalc(input) == 1 && "A single digit should return that same number");
+    char *input = "6";
+    assert(strCalc(input) == 6 && "A single digit should return that same number");
 }
 
 void testMultipleDigits(void)
 {
     // printf("strCalc('10') = %i", strCalc("10"));
-    char *input = "10";
-    assert(strCalc(input) == 10 && "multiple digits should return that same number");
+    char *input = "526";
+    assert(strCalc(input) == 526 && "multiple digits should return that same number");
+}
+
+void testCommaDelimitedSum(void){
+    char *input = "53,796";
+    assert(strCalc(input) == 849 && "Two numbers delimited by comma should return sum");
 }
